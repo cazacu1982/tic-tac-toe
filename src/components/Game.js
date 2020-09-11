26274eb25002843be04bfd15f5061757947c02c6
drca@netop.com <drca@netop.com>
2020-09-11 17:07:36 +0300

    add history

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,40 +1,56 @@
 import React, { useState } from 'react';
+import { calculateWinner } from '../utils';
 import Board from './Board';
 
+const styles = {
+  width: '200px',
+  margin: '20px auto',
+};
+
 const Game = () => {
-  const styles = {
-    display: 'block',
-    margin: '5px auto',
-    background: 'lightgrey',
-    border: '2px solid darkgrey',
-    fontSize: '40px',
-    fontWeight: '800',
-    cursor: 'pointer',
-    outline: 'none'
-  };
-  const [history, setHistory] = useState([Array(9).fill('')]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const [stepNumber, setStepNumber] = useState(0);
   const [xIsNext, setXisNext] = useState(true);
+  const winner = calculateWinner(history[stepNumber]);
   
   const handleClick = i => {
     const timeInHistory = history.slice(0, stepNumber + 1);
     const current = timeInHistory[stepNumber];
     const squares = [...current];
     // If user click an occupied square or if game is won, return
-    if (squares[i]) return;
+    if (winner || squares[i]) return;
     // Put an X or an O in the clicked square
     squares[i] = xIsNext ? 'X' : 'O';
     setHistory([...timeInHistory, squares]);
     setStepNumber(timeInHistory.length);
     setXisNext(!xIsNext);
+  }
+  
+  const jumpTo = step => {
+    setStepNumber(step);
+    setXisNext(step % 2 === 0)
   };
+  
+  const renderMoves = () => (
+    history.map((_step, move) => {
+      const destination = move ? `Go to move#${move}` : 'Go to start';
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{destination}</button>
+        </li>
+      )
+    })
+  )
   
   return (
     <>
       <Board squares={history[stepNumber]} onClick={handleClick} />
-      <button style= {styles} onClick={() => window.location.reload()}>{'refresh'}</button>
+      <div style={styles}>
+        <p>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+        {renderMoves()}
+      </div>
     </>
-  );
-};
+  )
+}
 
 export default Game;
